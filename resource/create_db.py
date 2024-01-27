4b62b4874206c001fae7d8df1c9e27ee462d7fbd
MrWolffy <mr.wolffy@qq.com>
2024-01-27 11:12:14 +0800

    update db definition

--- resource/create_db.py
+++ resource/create_db.py
@@ -12,29 +12,29 @@ data = pd.read_csv("m-league.csv")
 
 # region create table teams
 cur.execute("""
-CREATE TABLE teams(
-    team_name text PRIMARY KEY 
+CREATE TABLE Team(
+    team_name TEXT PRIMARY KEY 
 )
 """)
 for item in data["team"].unique().tolist():
-    cur.execute("INSERT INTO teams VALUES (?)", (item,))
+    cur.execute("INSERT INTO Team VALUES (?)", (item,))
 con.commit()
 # endregion
 
 # region create table players
 cur.execute("""
-CREATE TABLE players(
-    player_name text PRIMARY KEY 
+CREATE TABLE Player(
+    player_name TEXT PRIMARY KEY 
 )
 """)
 for item in data["player"].unique().tolist():
-    cur.execute("INSERT INTO players VALUES (?)", (item,))
+    cur.execute("INSERT INTO Player VALUES (?)", (item,))
 con.commit()
 # endregion
 
 # region create table players_teams
 cur.execute("""
-CREATE TABLE players_teams(
+CREATE TABLE Player_Team(
     player_name TEXT,
     team_name TEXT,
     season INTEGER,
@@ -43,7 +43,7 @@ CREATE TABLE players_teams(
 )
 """)
 for _, item in data[["player", "team", "stage"]].drop_duplicates().iterrows():
-    cur.execute("INSERT INTO players_teams VALUES (?, ?, ?)", (item["player"], item["team"], int(item["stage"][:4])))
+    cur.execute("INSERT INTO Player_Team VALUES (?, ?, ?)", (item["player"], item["team"], int(item["stage"][:4])))
 con.commit()
 # endregion
 
@@ -51,51 +51,41 @@ con.commit()
 import time
 
 cur.execute("""
-CREATE TABLE matches(
+CREATE TABLE Match(
     match_year INTEGER,
     match_month INTEGER,
     match_day INTEGER,
     match_no INTEGER,
-    round INTEGER,
-    PRIMARY KEY (match_year, match_month, match_day, match_no)
+    round INTEGER
 )
 """)
 for _, item in data[["date", "match_no", "round_no"]].drop_duplicates().iterrows():
     date_object = time.strptime(item["date"], "%Y/%m/%d")
-    cur.execute("INSERT INTO matches VALUES (?, ?, ?, ?, ?)",
+    cur.execute("INSERT INTO Match VALUES (?, ?, ?, ?, ?)",
                 (date_object.tm_year, date_object.tm_mon, date_object.tm_mday, item["match_no"], item["round_no"]))
 con.commit()
 # endregion
 
 # region create table players_matches
 cur.execute("""
-CREATE TABLE players_matches(
-    match_year INTEGER,
-    match_month INTEGER,
-    match_day INTEGER,
-    match_no INTEGER,
+CREATE TABLE Player_Match(
+    match_id INTEGER,
     player_name TEXT,
     point FLOAT,
     rank INTEGER check ( rank BETWEEN 1 AND 4),
     pt FLOAT,
-    FOREIGN KEY (match_year) REFERENCES matches(match_year),
-    FOREIGN KEY (match_month) REFERENCES matches(match_month),
-    FOREIGN KEY (match_day) REFERENCES matches(match_day),
-    FOREIGN KEY (match_no) REFERENCES matches(match_no),
+    FOREIGN KEY (match_id) REFERENCES matches(rowid),
     FOREIGN KEY (player_name) REFERENCES players(player_name)
 )
 """)
 cur.execute("""
     CREATE TRIGGER check_rank
-    BEFORE INSERT ON players_matches
+    BEFORE INSERT ON Player_Match
     FOR EACH ROW
     WHEN EXISTS(
         SELECT * 
-        FROM players_matches
-        WHERE match_year = NEW.match_year
-            AND match_month = NEW.match_month
-            AND match_day = NEW.match_day
-            AND match_no = NEW.match_no
+        FROM Player_Match
+        WHERE match_id = NEW.match_id
             AND((NEW.point < point AND NEW.rank < rank) OR(NEW.point > point AND NEW.rank > rank))
     ) 
         BEGIN
@@ -113,20 +103,22 @@ def calculate_pt(point, rank):
     return pt
 
 con.create_function("calculate_pt", 2, calculate_pt)
-# cur.execute("""
-#     CREATE TRIGGER check_pt
-#     BEFORE INSERT ON players_matches
-#     FOR EACH ROW
-#     WHEN ABS(calculate_pt(NEW.point, NEW.rank) - NEW.pt) > 0.001
-#         BEGIN
-#             SELECT RAISE(ABORT, 'Inconsistent pt.');
-#         END;
-# """)
+cur.execute("""
+    CREATE TRIGGER check_pt
+    BEFORE INSERT ON Player_Match
+    FOR EACH ROW
+    WHEN ABS(calculate_pt(NEW.point, NEW.rank) - NEW.pt) > 0.001
+        AND (SELECT COUNT(*) FROM Player_Match WHERE match_id = NEW.match_id AND rank = NEW.rank) > 1
+        BEGIN
+            SELECT RAISE(ABORT, 'Inconsistent pt.');
+        END;
+""")
 for _, item in data.iterrows():
     date_object = time.strptime(item["date"], "%Y/%m/%d")
-    cur.execute("insert into players_matches values (?, ?, ?, ?, ?, ?, ?, ?)",
-                (date_object.tm_year, date_object.tm_mon, date_object.tm_mday, item["match_no"],
-                 item["player"], item["point"], item["rank"], item["pt"]))
+    match_id = cur.execute("SELECT rowid FROM Match WHERE match_year = ? AND match_month = ? AND match_day = ? AND match_no = ?",
+                           (date_object.tm_year, date_object.tm_mon, date_object.tm_mday, item["match_no"])).fetchone()[0]
+    cur.execute("INSERT INTO Player_Match VALUES (?, ?, ?, ?, ?)",
+                (match_id, item["player"], item["point"], item["rank"], item["pt"]))
 con.commit()
 # endregion
 
